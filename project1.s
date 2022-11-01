d96cf5b1b282885e66764a76177af8186df10e87
Suprabhat Rijal <goodguysman@gmail.com>
2022-10-31 21:59:58 -0400

    Initialized registers for the loop

--- project1.s
+++ project1.s
@@ -10,6 +10,20 @@ la $a0, input
 li $a1, 11
 syscall
 
+
+# print new line
+li $v0, 4
+la $a0, nl
+syscall
+
+
+# initialize the loop
+li $s0, 0 # counter variable
+li $s1, 10 # the end of loop range
+la $s2, input # Address of the input string
+li $s3, 0 # sum of all numbers
+
+
 # exit
 li $v0, 10
 syscall
